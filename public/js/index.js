9ca6a842cf6da347c81ce9ddd8a8972b1c10dd6b
Contastic <conor@conor-mcgowan.com>
2019-05-09 21:55:41 -0500

    Logo Animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,28 @@
+var bouncingBall = anime({
+  targets: 'path.st3, path.st4',
+  translateY: '1.8vh',
+  duration: 400,
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeInCubic'
+});
+
+anime({
+  targets: 'path.st1 , path.st2',
+  rotateX: 35,
+  rotateY: 15,
+  scaleZ: 1,
+  scaleY: 1,
+  duration: 800,
+  loop: true,
+  direction: 'alternate',
+  easing: 'easeInOutSine'
+});
+
+
+
+
+
 // Button Functionality//
 $('.button-addon2').click(function(e){    
   $('#pg1').fadeOut('slow', function(){
