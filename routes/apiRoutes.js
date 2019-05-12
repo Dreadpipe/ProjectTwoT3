81a504ffe77fa3b4767383c423890efd1f77b5c9
Dreadpipe <benharris83@gmail.com>
2019-05-12 16:36:30 -0500

    updated api-routes for express

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,15 +10,21 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/cocktails", function(req, res) {
-    db.Cocktail.create(req.body).then(function(dbCocktail) {
+    db.Cocktail.create({
+      boozeType: req.body.boozeType,
+      drinkName: req.body.drinkName,
+      ingredients: req.body.ingredients,
+      instructions: req.body.instructions
+    }).then(function(dbCocktail) {
       res.json(dbCocktail);
     });
   });
 
+  // ------- DELETE API ROUTE BELOW----------
   // Delete an example by id
-  app.delete("/api/cocktails/:id", function(req, res) {
-    db.Cocktail.destroy({ where: { id: req.params.id } }).then(function(dbCocktail) {
-      res.json(dbCocktail);
-    });
-  });
+  // app.delete("/api/cocktails/:id", function(req, res) {
+  //   db.Cocktail.destroy({ where: { id: req.params.id } }).then(function(dbCocktail) {
+  //     res.json(dbCocktail);
+  //   });
+  // });
 };
