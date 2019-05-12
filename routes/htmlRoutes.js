3d7aa695c2335e82047e268be78cefc3106c07a6
Dreadpipe <benharris83@gmail.com>
2019-05-12 16:28:46 -0500

    updated htmlRoutes.js for express; removed handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,34 +4,51 @@ module.exports = function(app) {
   // Load index page
   // 
   app.get("/", function(req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        // examples: dbExamples
-      });
-    // });
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
   app.get("/form", function(req, res) {
-    // db.Example.findAll({}).then(function(dbExamples) {
-      res.render("example", {
-        msg: "Welcome!",
-        // examples: dbExamples
-      });
-    // });
+    res.sendFile(path.join(__dirname, "../public/form.html"));
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // blog route loads blog.html
+  app.get("/index", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
-  });
 };
+
+
+// ---------------- HANDLEBARS ROUTES BELOW------------------
+//   app.get("/", function(req, res) {
+//     // db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("index", {
+//         msg: "Welcome!",
+//         // examples: dbExamples
+//       });
+//     // });
+//   });
+
+//   app.get("/form", function(req, res) {
+//     // db.Example.findAll({}).then(function(dbExamples) {
+//       res.render("example", {
+//         msg: "Welcome!",
+//         // examples: dbExamples
+//       });
+//     // });
+//   });
+
+//   // Load example page and pass in an example by id
+//   app.get("/example/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
